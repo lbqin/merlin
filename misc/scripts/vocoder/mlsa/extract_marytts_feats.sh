1abb71a80eee84c252b95b115fdc3392bcc5ca9b
vsooda <liushouda@qq.com>
2016-10-28 19:59:55 +0800

    fix nj

--- misc/scripts/vocoder/mlsa/extract_marytts_feats.sh
+++ misc/scripts/vocoder/mlsa/extract_marytts_feats.sh
@@ -1,13 +1,13 @@
-corpus_dir=/home/sooda/data/tts/labixx120_48k/
+corpus_dir=/home/sooda/data/tts/xll_48k/
 audio_dir=$corpus_dir/wav 
 cppmary_base=/home/sooda/speech/cppmary/
 cppmary_bin=$cppmary_base/build/
 sample_rate=48000
 FRAMESHIFT=0.005
 nj=8
-featdir=/tmp/acoustic
+featdir=/tmp/acoustic_data
 mgcdir=$featdir/mgc
-strdir=$featdir/str
+strdir=$featdir/bap
 lf0dir=$featdir/lf0
 
 rm -rf $mgcdir $strdir $lf0dir
@@ -17,7 +17,7 @@ makeid="xargs -i basename {} .wav"
 
 find $audio_dir -name "*.wav" | sort | $makeid | awk -v audiodir=$audio_dir '{line=$1" "audiodir"/"$1".wav"; print line}' > $corpus_dir/wav.scp
 
-./make_parallel.sh --sample-frequency $sample_rate $corpus_dir /tmp $lf0dir "./compute-lf0-feats.sh" || exit 1
+./make_parallel.sh --sample-frequency $sample_rate --nj $nj $corpus_dir /tmp $lf0dir "./compute-lf0-feats.sh" || exit 1
 ./make_parallel.sh --sample-frequency $sample_rate --nj $nj $corpus_dir /tmp $strdir "./compute-str-feats.sh" || exit 1
-./make_parallel.sh --sample-frequency $sample_rate $corpus_dir /tmp $mgcdir "./compute-mgc-feats.sh" || exit 1
+./make_parallel.sh --sample-frequency $sample_rate --nj $nj $corpus_dir /tmp $mgcdir "./compute-mgc-feats.sh" || exit 1
 
