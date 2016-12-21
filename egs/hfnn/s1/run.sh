277a16a59b79d3eaf94ad88d1ef846db55e6829f
vsooda <liushouda@qq.com>
2016-12-21 16:13:46 +0800

    1. merge setup.sh into run.sh; no need setup.sh any more; edit the global_settings.cfg derectly 2. remove prepare test sys sh 3. remove voice config file

--- egs/hfnn/s1/run.sh
+++ egs/hfnn/s1/run.sh
@@ -7,8 +7,31 @@ fi
 
 ### Step 1: setup directories and the training data files ###
 echo "Step 1: setting up experiments directory and the training data files..."
+voice_name=hfnn
+current_working_dir=$(pwd)
+merlin_dir=$(dirname $(dirname $(dirname $current_working_dir)))
+experiments_dir=${current_working_dir}/experiments
+voice_dir=${experiments_dir}/${voice_name}
+acoustic_dir=${voice_dir}/acoustic_model
+duration_dir=${voice_dir}/duration_model
+synthesis_dir=${voice_dir}/test_synthesis
+
+mkdir -p ${experiments_dir}
+mkdir -p ${voice_dir}
+mkdir -p ${acoustic_dir}
+mkdir -p ${duration_dir}
+mkdir -p ${synthesis_dir}
+
+data_dir=${current_working_dir}/${voice_name}_data
+
+if [[ ! -d ${acoustic_dir}/data ]]; then
+    ln -s ${data_dir}/data ${acoustic_dir}/data
+    ln -s ${data_dir}/data ${duration_dir}/data
+    cp -r ${data_dir}/test_data/* ${synthesis_dir}/
+fi
+echo "data is ready!"
+
 global_config_file=conf/global_settings.cfg
-./scripts/setup.sh hfnn
 ./scripts/prepare_config_files.sh $global_config_file 0
 ./scripts/prepare_config_files.sh $global_config_file 1
 
